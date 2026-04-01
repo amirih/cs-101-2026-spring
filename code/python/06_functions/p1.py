f3c4f2f60ff3bd61c129a8ee329b38e9b16270eb
Hossein Amiri <hossein.amiri.enterprise@gmail.com>
2026-03-31 22:12:38 -0400

    Add pseudocode comments to p1.py
    Added pseudocode comments for function outline.

--- code/python/06_functions/p1.py
+++ code/python/06_functions/p1.py
@@ -1,6 +1,11 @@
 def print_hello():
     print("Hello, world!")
 
+# Python-style pseudocode
+#function pseudocode(input):
+    #recap()
+    #return remembered()
+
 if __name__ == '__main__':
 
     print_hello()
